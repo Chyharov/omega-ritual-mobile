0c00551e49243ac39b3ea653f787dbcde8dc041a
myusername <chyharov@gmail.com>
2023-04-21 19:31:21 +0300

    added new items for car park list

--- src/components/RitualCarParkList/RitualCarParkList.jsx
+++ src/components/RitualCarParkList/RitualCarParkList.jsx
@@ -1,12 +1,25 @@
 import s from '../RitualCarParkList/RitualCarParkList.module.scss';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-import car from '../../images/whiteVolksWagen/photo_1_2023-04-20_20-12-55.jpg';
-import car1 from '../../images/whiteVolksWagen/photo_2_2023-04-20_20-12-55.jpg';
-import car2 from '../../images/whiteVolksWagen/photo_3_2023-04-20_20-12-55.jpg';
-import car3 from '../../images/whiteVolksWagen/photo_4_2023-04-20_20-12-55.jpg';
-import car4 from '../../images/whiteVolksWagen/photo_5_2023-04-20_20-12-55.jpg';
-import car5 from '../../images/whiteVolksWagen/photo_6_2023-04-20_20-12-55.jpg';
+import carVW from '../../images/whiteVolksWagen/photo_5_2023-04-20_20-12-55.jpg';
+import carVW2 from '../../images/whiteVolksWagen/photo_3_2023-04-20_20-12-55.jpg';
+import carVW3 from '../../images/whiteVolksWagen/photo_1_2023-04-20_20-12-55.jpg';
+import carVW4 from '../../images/whiteVolksWagen/photo_4_2023-04-20_20-12-55.jpg';
+import carVW5 from '../../images/whiteVolksWagen/photo_6_2023-04-20_20-12-55.jpg';
+import carVW6 from '../../images/whiteVolksWagen/photo_2_2023-04-20_20-12-55.jpg';
+
+import carWM from '../../images/whiteMercedes/photo_5_2023-04-20_20-13-44.jpg';
+import carWM2 from '../../images/whiteMercedes/photo_7_2023-04-20_20-13-44.jpg';
+import carWM3 from '../../images/whiteMercedes/photo_12_2023-04-20_20-13-44.jpg';
+import carWM4 from '../../images/whiteMercedes/photo_10_2023-04-20_20-13-44.jpg';
+
+import carBM from '../../images/blackMercedes/photo_1_2023-04-20_20-13-21.jpg';
+import carBM2 from '../../images/blackMercedes/photo_2_2023-04-20_20-13-21.jpg';
+import carBM3 from '../../images/blackMercedes/photo_14_2023-04-20_20-13-44.jpg';
+import carBM4 from '../../images/blackMercedes/photo_10_2023-04-20_20-13-21.jpg';
+import carBM5 from '../../images/blackMercedes/photo_4_2023-04-20_20-13-21.jpg';
+import carBM6 from '../../images/blackMercedes/photo_4_2023-04-20_20-13-44.jpg';
+import carBM7 from '../../images/blackMercedes/photo_9_2023-04-20_20-13-44.jpg';
 
 const RitualCarParkList = () => {
   return (
@@ -29,15 +42,70 @@ const RitualCarParkList = () => {
                     visibleSlides={1}
                 >
                     <Slider>
-                    <Slide index={0}><img src={car4} alt="ritualCar" /></Slide>
-                    <Slide index={1}><img src={car2} alt="ritualCar" /></Slide>
-                    <Slide index={2}><img src={car} alt="ritualCar" /></Slide>
-                    <Slide index={3}><img src={car3} alt="ritualCar" /></Slide>
-                    <Slide index={4}><img src={car5} alt="ritualCar" /></Slide>
-                    <Slide index={4}><img src={car1} alt="ritualCar" /></Slide>
+                    <Slide index={0}><img src={carVW} alt="ritualCar" /></Slide>
+                    <Slide index={1}><img src={carVW2} alt="ritualCar" /></Slide>
+                    <Slide index={2}><img src={carVW3} alt="ritualCar" /></Slide>
+                    <Slide index={3}><img src={carVW4} alt="ritualCar" /></Slide>
+                    <Slide index={4}><img src={carVW5} alt="ritualCar" /></Slide>
+                    <Slide index={4}><img src={carVW6} alt="ritualCar" /></Slide>
                     </Slider>
 
-                    <p className={s.ritualCarParkList__description}>Volkswagen Т-5 Білий</p>
+                    <p className={s.ritualCarParkList__name}>Volkswagen Т-5 Білий</p>
+                    <p className={s.ritualCarParkList__description}>(2 - 5 пасажирів)</p>
+
+                    <div className={s.carouselButtonCenter}>
+                        <ButtonBack className={s.carouselButton}>Назад</ButtonBack>
+                        <ButtonNext className={s.carouselButton}>Вперед</ButtonNext>
+                    </div>
+                </CarouselProvider>
+            </li>
+
+            <li className={s.ritualCarParkList}>
+                <p className={s.ritualCarParkList__item}>Середній клас</p>
+                <CarouselProvider
+                    naturalSlideWidth={280}
+                    naturalSlideHeight={210}
+                    totalSlides={4}
+                    visibleSlides={1}
+                >
+                    <Slider>
+                    <Slide index={0}><img src={carWM} alt="ritualCar" /></Slide>
+                    <Slide index={1}><img src={carWM2} alt="ritualCar" /></Slide>
+                    <Slide index={2}><img src={carWM3} alt="ritualCar" /></Slide>
+                    <Slide index={3}><img src={carWM4} alt="ritualCar" /></Slide>
+
+                    </Slider>
+
+                    <p className={s.ritualCarParkList__name}>Mercedes Benz Sprinter Білий</p>
+                    <p className={s.ritualCarParkList__description}>(14 - 18 пасажирів)</p>
+
+                    <div className={s.carouselButtonCenter}>
+                        <ButtonBack className={s.carouselButton}>Назад</ButtonBack>
+                        <ButtonNext className={s.carouselButton}>Вперед</ButtonNext>
+                    </div>
+                </CarouselProvider>
+            </li>
+
+            <li className={s.ritualCarParkList}>
+                <p className={s.ritualCarParkList__item}>VIP клас</p>
+                <CarouselProvider
+                    naturalSlideWidth={280}
+                    naturalSlideHeight={210}
+                    totalSlides={7}
+                    visibleSlides={1}
+                >
+                    <Slider>
+                    <Slide index={0}><img src={carBM} alt="ritualCar" /></Slide>
+                    <Slide index={1}><img src={carBM2} alt="ritualCar" /></Slide>
+                    <Slide index={2}><img src={carBM3} alt="ritualCar" /></Slide>
+                    <Slide index={3}><img src={carBM4} alt="ritualCar" /></Slide>
+                    <Slide index={4}><img src={carBM5} alt="ritualCar" /></Slide>
+                    <Slide index={4}><img src={carBM6} alt="ritualCar" /></Slide>
+                    <Slide index={4}><img src={carBM7} alt="ritualCar" /></Slide>
+                    </Slider>
+
+                    <p className={s.ritualCarParkList__name}>Mercedes Benz Sprinter Чорний</p>
+                    <p className={s.ritualCarParkList__description}>(14 - 18 пасажирів)</p>
 
                     <div className={s.carouselButtonCenter}>
                         <ButtonBack className={s.carouselButton}>Назад</ButtonBack>
