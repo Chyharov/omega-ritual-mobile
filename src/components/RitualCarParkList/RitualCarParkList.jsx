24164d7eaf0a854683a27311627a5ce138e61324
myusername <chyharov@gmail.com>
2023-04-23 09:44:43 +0300

    fix modal useState

--- src/components/RitualCarParkList/RitualCarParkList.jsx
+++ src/components/RitualCarParkList/RitualCarParkList.jsx
@@ -86,7 +86,6 @@ const RitualCarParkList = () => {
     <p className={s.ritualCarPark__description}>Саме слово «катафалк» буквально означає постамент для встановлення труни з покійним під час похорону. Однак у нашій мові воно набуло дещо іншого значення – транспорт для ритуального перевезення покійного під час похоронної церемонії.</p>
     <p className={s.ritualCarPark__description}>Померлих військових та монарших осіб перевозили під час похорону на гарматних лафетах або запряжених у кінні упряжки возах, прикрашених квітами. Сьогодні функції ритуального транспорту виконують автомобілі катафалки. Це можуть бути як легкові машини із задніми дверима, через які завантажується труна, так і місткі мікро- або повноцінні автобуси з можливістю посадки рідних та близьких покійного.</p>
     <p className={s.ritualCarPark__description}>У ритуальному бюро похоронний дім «Омега» ви можете замовити у будь-яку точку Києва та Київської області один із чотирьох видів катафалків. Ціна таких послуг залежить від класу ритуального транспорту та його місткості. Це може бути як бюджетний економ варіант, так і катафалк VIP-рівня.</p>
-    
       <div className={s.ritualCarPark__container}>
         <ul>
           <li className={s.ritualCarParkList}>
@@ -113,7 +112,7 @@ const RitualCarParkList = () => {
                   </div>
               </CarouselProvider>
           </li>
-          {modalImage && <Modal image={modalImage} closeModal={closeModal} currentSlide={RitualCarParkWVWPhoto.indexOf(modalImage)} />}
+          {isOpen && modalImage && <Modal image={modalImage} closeModal={closeModal} currentSlide={RitualCarParkWVWPhoto.indexOf(modalImage)} />}
         </ul>
       </div>
     </section>
