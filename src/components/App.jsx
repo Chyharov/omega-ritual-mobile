a32fbca0acd64544799ad10fc4166e88e3d6ecbb
myusername <chyharov@gmail.com>
2023-07-05 15:47:32 +0300

    ADDED: loading lazy for pages on App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,38 @@
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import MainPage from 'pages/MainPage/MainPage';
-import WreathsPage from 'pages/WreathsPage/WreathsPage';
-import CoffinsPage from 'pages/CoffinsPage/CoffinsPage';
-import CrossesPage from 'pages/CrossesPage/CrossesPage';
-import CoveredPage from 'pages/CoveredPage/CoveredPage';
-import OtherAttributesPage from 'pages/OtherAttributesPage/OtherAttributesPage';
-import ArtificialWreathsPage from 'pages/ArtificialWreathsPage/ArtificialWreathsPage';
-import FreshFlowersWreathPage from 'pages/FreshFlowersWreathPage/FreshFlowersWreathPage';
-import FuneralVasesPage from 'pages/FuneralVasesPage/FuneralVasesPage';
-import DecorationsPage from 'pages/DecorationsPage/DecorationsPage';
-import FuneralPortraitPage from 'pages/FuneralPortraitPage/FuneralPortraitPage';
-import FuneralSetPage from 'pages/FuneralSetPage/FuneralSetPage';
 
+const MainPage = lazy(() => import('pages/MainPage/MainPage'));
+const WreathsPage = lazy(() => import('pages/WreathsPage/WreathsPage'));
+const CoffinsPage = lazy(() => import('pages/CoffinsPage/CoffinsPage'));
+const CrossesPage = lazy(() => import('pages/CrossesPage/CrossesPage'));
+const CoveredPage = lazy(() => import('pages/CoveredPage/CoveredPage'));
+const OtherAttributesPage = lazy(() => import('pages/OtherAttributesPage/OtherAttributesPage'));
+const ArtificialWreathsPage = lazy(() => import('pages/ArtificialWreathsPage/ArtificialWreathsPage'));
+const FreshFlowersWreathPage = lazy(() => import('pages/FreshFlowersWreathPage/FreshFlowersWreathPage'));
+const FuneralVasesPage = lazy(() => import('pages/FuneralVasesPage/FuneralVasesPage'));
+const DecorationsPage = lazy(() => import('pages/DecorationsPage/DecorationsPage'));
+const FuneralPortraitPage = lazy(() => import('pages/FuneralPortraitPage/FuneralPortraitPage'));
+const FuneralSetPage = lazy(() => import('pages/FuneralSetPage/FuneralSetPage'));
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/wreaths" element={<WreathsPage />} />
-        <Route path="/coffins" element={<CoffinsPage />} />
-        <Route path="/crosses" element={<CrossesPage />} />
-        <Route path="/covered" element={<CoveredPage />} />
-        <Route path="/otherattributes" element={<OtherAttributesPage />} />
-        <Route path="/artificialwreaths" element={<ArtificialWreathsPage />} />
-        <Route path="/freshflowerswreath" element={<FreshFlowersWreathPage />} />
-        <Route path="/vases" element={<FuneralVasesPage />} />
-        <Route path="/decorations" element={<DecorationsPage />} />
-        <Route path="/funeralportrait" element={<FuneralPortraitPage />} />
-        <Route path="/funeralset" element={<FuneralSetPage/>} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/wreaths" element={<WreathsPage />} />
+          <Route path="/coffins" element={<CoffinsPage />} />
+          <Route path="/crosses" element={<CrossesPage />} />
+          <Route path="/covered" element={<CoveredPage />} />
+          <Route path="/otherattributes" element={<OtherAttributesPage />} />
+          <Route path="/artificialwreaths" element={<ArtificialWreathsPage />} />
+          <Route path="/freshflowerswreath" element={<FreshFlowersWreathPage />} />
+          <Route path="/vases" element={<FuneralVasesPage />} />
+          <Route path="/decorations" element={<DecorationsPage />} />
+          <Route path="/funeralportrait" element={<FuneralPortraitPage />} />
+          <Route path="/funeralset" element={<FuneralSetPage />} />
+        </Routes>
+      </Suspense>
     </>
-    
-    
   );
 };
